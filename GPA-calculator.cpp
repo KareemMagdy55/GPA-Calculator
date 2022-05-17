899dfa360803b9d49009e5a122f4da67aa3018b5
KareemMagdy55 <91559979+KareemMagdy55@users.noreply.github.com>
2022-05-17 22:05:23 +0200

    Improve the code's efficiency and elegance.
    1. Use vectors instead of arrays to store user data dynamically.
    
    2. Reform grades loop - remove parentheses to clarify the relation between points and grades ,also overloading to lower function to help in writing only one condition in each "if".
    
    3. Rewrite the entire code by create a prototype of the program's main function also,  make the main function above all other functions.

--- GPA-calculator.cpp
+++ GPA-calculator.cpp
@@ -1,60 +1,77 @@
 #include <iostream>
+#include <vector>
 using namespace std ;
 
-void gpaCalculator(){
-    
-    int number_of_subjects = 0 ;
-    cout<< "enter number of subjects :" ;
-    cin >> number_of_subjects;
-
-    string subjects[number_of_subjects] = {};
-    string grades[number_of_subjects] = {};
-    int credit_hours [ number_of_subjects ]= {};
-    float point [number_of_subjects ]= {};
-
-    for (int i = 0 ; i < number_of_subjects ; i+=1){
-        cout << "enter subject number "<< i + 1 << " :" ; cin >> subjects[i];}
-
-    for (int i = 0 ; i < number_of_subjects ; i++){
-        cout << "enter " << subjects[i] << " grade :"; cin >> grades[i];
-        cout << "enter " << subjects[i] << " credit hours :"; cin >> credit_hours[i];}
-    
-    for (int i = 0 ; i < number_of_subjects ; i++) {
-        if (grades[i]=="A+" || grades[i]=="a+" )
-        {    point [i]=4;}
-        else if (grades[i]=="A" || grades[i]=="a")
-        {    point [i]=3.7;}
-        else if (grades[i]=="B+" || grades[i]=="b+")
-        {    point [i]=3.3;}
-        else if (grades[i]=="B" || grades[i]=="b")
-        {    point [i]=3;}
-        else if (grades[i]=="C+" || grades[i]=="c+")
-        {   point [i]=2.7;}
-        else if (grades[i]=="C" || grades[i]=="c")
-        {  point [i]=2.4;}
-        else if (grades[i]=="D+" || grades[i]=="d+")
-        { point [i]=2.2;}
-        else if (grades[i]=="D" || grades[i]=="d")
-        {   point [i]=2;}
-        else
-        {  point [i] =0;}
-    }
-
-
-    float sum = 0 ;
-    int total_hours = 0 ;
-
-    for (int i = 0 ; i < number_of_subjects ; i+=1){
-        sum = sum + (point[i] * credit_hours[i] );
-        total_hours = total_hours + credit_hours [i] ;
-    }
-
-    cout << "GPA is " << sum / total_hours ;
-}
+void gpaCalculator();
 
 int main() {
-    
+
     gpaCalculator() ;
 
     return 0;
 }
+
+
+void tolower(string & word){
+    for(char&c : word){
+        c = tolower(c);
+    }
+}
+
+void gpaCalculator(){
+
+    int numberOfSubjects = 0 ;
+    cout<< "enter number of subjects :" ;
+    cin >> numberOfSubjects;
+
+
+    vector <string> subjects(numberOfSubjects);
+    vector <string> grades(numberOfSubjects);
+    vector <int> creditHours(numberOfSubjects);
+    vector <float> point(numberOfSubjects);
+
+    for (int i = 0 ; i < numberOfSubjects ; i++){
+        cout << "enter subject number "<< i + 1 << " :" ;
+        cin >> subjects[i];
+    }
+
+    for (int i = 0 ; i < numberOfSubjects ; i++){
+        cout << "enter " << subjects[i] << " grade :";
+        cin >> grades[i];
+        cout << "enter " << subjects[i] << " credit hours :";
+        cin >> creditHours[i];
+    }
+
+    for (int i = 0 ; i < numberOfSubjects ; i++) {
+        tolower(grades[i]);
+        if ( grades[i]=="a+" ) point[i]=4;
+
+        else if ( grades[i]=="a") point[i]=3.7;
+
+        else if (grades[i]=="b+") point[i]=3.3;
+
+        else if (grades[i]=="b") point[i]=3;
+
+        else if ( grades[i]=="c+")  point[i]=2.7;
+
+        else if (grades[i]=="c")  point[i]=2.4;
+
+        else if (grades[i]=="d+") point[i]=2.2;
+
+        else if ( grades[i]=="d") point[i]=2;
+
+        else point[i] =0;
+    }
+
+    float sum = 0 ;
+    int totalHours = 0 ;
+
+    for (int i = 0 ; i < numberOfSubjects ; i+=1){
+        sum += (point[i] * (float)creditHours[i] );
+        totalHours +=  creditHours[i] ;
+    }
+
+    cout << "GPA is " << sum / (float)totalHours ;
+}
+
+
